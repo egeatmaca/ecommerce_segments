5f4ed284facff61df1048a76b8074962e70efa47
egeatmaca <egeatmaca98@gmail.com>
2025-01-15 20:42:45 +0100

    orders eda + typo fix

--- sql/pipeline_users_enriched.sql
+++ sql/pipeline_users_enriched.sql
@@ -6,7 +6,7 @@ WITH user_purchases AS (
     SELECT user_id, 
         COUNT(*) AS purchases, 
         SUM(sale_price) AS revenue,
-        MIN(created_at) AS fist_purchase_date,
+        MIN(created_at) AS first_purchase_date,
         MAX(created_at) AS last_purchase_date,
         ARRAY_AGG(product_category) AS purchased_categories
     FROM order_items_enriched
@@ -27,7 +27,7 @@ WITH user_purchases AS (
         COALESCE(up.revenue, 0) AS revenue,
         up.purchased_categories,
         u.created_at,
-        up.fist_purchase_date,
+        up.first_purchase_date,
         up.last_purchase_date
     FROM users AS u
     LEFT JOIN user_purchases AS up
--- sql/scratchpad.sql
+++ sql/scratchpad.sql
@@ -2,4 +2,7 @@
 -- SELECT COUNT(*) FROM order_items_enriched;
 
 
-SELECT * FROM users_enriched LIMIT 100;
+-- SELECT * FROM users_enriched LIMIT 100;
+
+-- DROP TABLE order_items_enriched;
+-- DROP TABLE users_enriched;
